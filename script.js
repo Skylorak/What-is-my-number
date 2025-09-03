060282b71a3993c262487c6bd1a66079aae5e4d5
yeah <skailorak@gmail.com>
2025-09-03 23:21:39 +0200

    Fixed mistake in score.

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.querySelector('.check').addEventListener('click', () => {
     } else {
       /* Fifth scenario (When the player lost)*/
       document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector('.score').textContent = 20;
     }
   }
 });
