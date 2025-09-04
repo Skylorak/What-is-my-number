c651fec96466b2c0d60cb404f126ec27e3a8c575
yeah <skailorak@gmail.com>
2025-09-04 16:20:44 +0200

    Refactor code and use the DRY principle

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
 
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const disableInput = boolean => {
+  document.querySelector('.your-number').disabled = boolean;
+};
+
+document.querySelector('.your-number').addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    document.querySelector('.check').click();
+  }
+});
+
 document.querySelector('.check').addEventListener('click', () => {
   const value = document.querySelector('.your-number').value;
   const userNumber = Number(value);
@@ -13,21 +27,19 @@ document.querySelector('.check').addEventListener('click', () => {
 
   /* When value is empty */
   if (value === '') {
-    document.querySelector('.message').textContent = 'Please write a number';
+    displayMessage('Please write a number');
     /* When the number is greater than 20 */
   } else if (userNumber > 20) {
-    document.querySelector('.message').textContent =
-      'The number is greater than 20';
+    displayMessage('The number is greater than 20');
     /* When the number is less than 1 */
   } else if (userNumber < 1) {
-    document.querySelector('.message').textContent =
-      'The number is less than 1';
+    displayMessage('The number is less than 1');
     /* When the player wins */
   } else if (userNumber === secretNumber) {
-    document.querySelector('.message').textContent = 'You got it!';
+    displayMessage('You got it!');
 
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.your-number').disabled = true;
+    disableInput(true);
 
     /* highscore logic  */
     if (score > highscore) {
@@ -38,30 +50,18 @@ document.querySelector('.check').addEventListener('click', () => {
     /* styles */
     document.querySelector('body').style.backgroundColor = '#50C878';
     document.querySelector('.number').style.width = '200px';
-    /* When the number is too high */
-  } else if (userNumber > secretNumber) {
+    /* When guess is wrong */
+  } else if (userNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      displayMessage(userNumber > secretNumber ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost!';
+      displayMessage('You lost!');
       document.querySelector('.score').textContent = 0;
-      document.querySelector('.your-number').disabled = true;
+      disableInput(true);
 
       /* styles */
-      document.querySelector('body').style.backgroundColor = '#D22B2B';
-    }
-    /* When the number is too low */
-  } else if (userNumber < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.your-number').disabled = true;
       document.querySelector('body').style.backgroundColor = '#D22B2B';
     }
   }
@@ -73,10 +73,11 @@ document.querySelector('.again').addEventListener('click', () => {
   secretNumber = Math.trunc(Math.random() * 20 + 1);
 
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Enter your guess!';
+  displayMessage('Enter your guess!');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.your-number').value = '';
-  document.querySelector('.your-number').disabled = false;
+  disableInput(false);
+
   /* styles */
   document.querySelector('body').style.backgroundColor = 'black';
   document.querySelector('.number').style.width = '100px';
